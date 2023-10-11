d5b9f80e049d0a6b7e7ec7bf60e375ecf1f64e1b
Patryk Szlachta <szlachcikk@gmail.com>
2023-10-11 20:48:43 +0100

    fixed mobile navigation

--- Js/main.js
+++ Js/main.js
@@ -1,22 +1,24 @@
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer__year');
-
+const allNavItems = document.querySelectorAll('.nav__link-mobile');
 
 const handleNav = () => {
-    navBtn.classList.toggle('is-active')
-    navMobile.classList.toggle('nav-mobile--active')
-}
+	navBtn.classList.toggle('is-active');
+	navMobile.classList.toggle('nav-mobile--active');
 
-navBtn.addEventListener('click', handleNav)
-
+	allNavItems.forEach((item) => {
+		item.addEventListener('click', () => {
+            navBtn.classList.remove('is-active');
+            navMobile.classList.remove('nav-mobile--active');
+        });
+	});
+};
+navBtn.addEventListener('click', handleNav);
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear();
 	footerYear.innerText = year;
 };
-
-
-
 
 handleCurrentYear();
